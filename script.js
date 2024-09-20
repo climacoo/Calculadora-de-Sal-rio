d3cc8bd4b1810b18db5cb190171f65139f4fe8df
climacoo <135074513+climacoo@users.noreply.github.com>
2024-09-19 21:15:19 -0300

    Add files via upload
    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,30 @@
+function calcularSalario() {
+    
+    let salarioBase = parseFloat(document.getElementById("salario").value);
+    let cargo = document.getElementById("cargo").value;
+    let aumento;
+    let percentual;
+
+    
+    switch (cargo) {
+        case "diretor":
+            percentual = 0.10; 
+            break;
+        case "secretario":
+            percentual = 0.20; 
+            break;
+        case "operador":
+            percentual = 0.30; 
+            break;
+        default:
+            percentual = 0;
+    }
+
+    
+    aumento = salarioBase * percentual;
+    let salarioAtualizado = salarioBase + aumento;
+
+    
+    document.getElementById("resultado").innerHTML = 
+        `O novo salário é R$${salarioAtualizado.toFixed(2)} com um aumento de ${percentual * 100}%`;
+}
